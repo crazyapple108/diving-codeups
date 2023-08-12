dbb395688c04e8d4f893fce9fcba3a16830778ed
crazyapple108 <crazyapple108@gmail.com>
2023-08-13 07:38:09 +0900

    blog voice section 作成

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  $(function(){
+    $('.btn-trigger').on('click', function() {
+      $(this).toggleClass('active');
+      return false;
+    });
+  });
 
 });
